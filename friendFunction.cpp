73ec3b16de24843ad22244c83d558583561dbc06
sanika-bhor <bhorsanika0239@gmail.com>
2024-10-29 21:42:11 +0530

    changes

---
+++ friendFunction.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+class Point
+{
+    private:
+        int x,y;
+    public:
+        Point()
+        {
+            this->x=0;
+            this->y=0;
+        }
+        Point()
+        {
+            this->x = 0;
+            this->y = 0;
+        }
+};
