fe1a8bcb3d20b1b2721bb11a4652211103f655fc
sanika-bhor <bhorsanika0239@gmail.com>
2024-11-06 22:30:27 +0530

    method overloading

--- methodOverloading.cpp
+++ methodOverloading.cpp
@@ -1,10 +1,12 @@
 /* 
-write a c++ program to implement method overloading have class name as 'mathEngine' perform addition by overloading add() 
+write a c++ program to implement method overloading have class name as 'mathEngine' perform addition,subtraction by overloading add() & sub() 
 also implement constructor overloading. create dynamic memory allocation.
 -------------------------------------------------------------------
     class : mathEngine
  function : add(n1,n2)
-             add(n1,n2,n3)
+            add(n1,n2,n3)
+            sub(n1,n2)
+            sub(n1,n2,n3)
 constructor: default
              parameterized
 */
@@ -24,9 +26,9 @@ class mathEngine
     // parameterized constructor
     mathEngine(int num1, int num2)
     {
-        cout << "\nMath Engine is created with two numbers" << endl;
+        cout << "\n\nMath Engine is created with two numbers" << endl;
     }
-    // method overloadding
+    // method overloadding:add()
     int add(int num1, int num2)
     {
         return num1+num2;
@@ -35,20 +37,37 @@ class mathEngine
     {
         return num1+num2+num3;
     }
+
+    // method overloadding:sub()
+     int sub(int num1, int num2)
+    {
+        return num1-num2;
+    }
+    int sub(int num1, int num2, int num3)
+    {
+        return num1-num2-num3;
+    }
 };
 int main()
 {
     // dynamic memory allocation:
     // it create object on heap memory and stores the address of stack memory
     mathEngine *ptrMathEngine=new mathEngine();
+    // add()
     int result1 = ptrMathEngine->add(20, 10);
-    cout<<"\nresult of add function having 2 parameters: "<<result1;
+    cout<<"result of add function having 2 parameters: "<<result1;
     int result2 = ptrMathEngine->add(10, 20, 30);
     cout << "\nresult of add function having 2 parameters: " << result2;
 
+     // sub()
+    int subResult1 = ptrMathEngine->sub(20, 10);
+    cout<<"\nresult of add function having 2 parameters: "<<subResult1;
+    int subResult2 = ptrMathEngine->sub(10, 20, 30);
+    cout << "\nresult of add function having 2 parameters: " << subResult2;
+
     mathEngine *ptrMathEngine2 = new mathEngine(10, 20);
    int result3= ptrMathEngine2->add(56, 78);
-    cout << "\nresult of add function having 2 parameters: " << result3;
+    cout << "result of add function having 2 parameters: " << result3;
 
     int result4=ptrMathEngine2->add(25, 2, 96);
     cout << "\nresult of add function having 2 parameters: " << result4;
