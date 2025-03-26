37ff54889b19b2f5b7f7e3772d58c6fcb4b9e78e
sanika-bhor <bhorsanika0239@gmail.com>
2025-03-26 17:57:48 +0530

    banking application for serialize and deserialize

--- Serialization.cpp
+++ Serialization.cpp
@@ -1,97 +1,138 @@
-#include<iostream>
+#include <iostream>
+#include <fstream>
 using namespace std;
 
 namespace Bank
 {
     class Account
     {
-        private:
-             int accountNumber;
-             double balance;
-             string AccountHolder;
-        public:
-            Account()
-            {
-                accountNumber = 0;
-                balance = 0.0;
-                AccountHolder = "";
-            }
+    private:
+        int accountNumber;
+        double balance;
+        string AccountHolder;
 
-            Account(int accno, double bal, string hln)
-            {
-                accountNumber = accno;
-                balance = bal;
-                AccountHolder = hln;
-            }
+    public:
+        Account()
+        {
+            accountNumber = 0;
+            balance = 0.0;
+            AccountHolder = "";
+        }
 
-            void deposit(double amount)
+        Account(int accno, double bal, string hln)
+        {
+            accountNumber = accno;
+            balance = bal;
+            AccountHolder = hln;
+        }
+
+        void deposit(double amount)
+        {
+            if (amount > 0)
             {
-                if(amount>0)
-                {
                 balance += amount;
                 cout << "Amount deposited successfully.\n";
-                }
-                else{
-                    cout << "Invalid amount.\n";
-                }
             }
-
-
-            void withdraw(double amount)
+            else
             {
-                if(amount>0 && balance>=amount)
-                {
-                    balance -= amount;
-                    cout << "Amount withdrawn successfully.\n";
-                }
-                else
-                {
-                    cout<<"insufficient amount"<<endl;
-                }
+                cout << "Invalid amount.\n";
             }
+        }
 
-            double checkBalance()
+        void withdraw(double amount)
+        {
+            if (amount > 0 && balance >= amount)
             {
-                cout<<"\n current available balance :"<<balance<<endl;
-                return balance;
+                balance -= amount;
+                cout << "Amount withdrawn successfully.\n";
             }
-
-            
-
-
-            friend istream& operator>>(istream& is, Account& acc)
+            else
             {
-                is>>acc.accountNumber>>acc.balance>>acc.AccountHolder;
-                return is;
+                cout << "insufficient amount" << endl;
             }
+        }
 
-            friend ostream &operator<<(ostream &os, Account &acc)
-            {
-                os << acc.accountNumber << "  " << acc.balance << "  " << acc.AccountHolder;
-                return os;
-            }
+        double checkBalance()
+        {
+            cout << "\n current available balance :" << balance << endl;
+            return balance;
+        }
 
-            void display()
-            {
-                cout << "\n Account Number : " << accountNumber;
-                cout << "\n Account Holder : " << AccountHolder;
-                cout << "\n Balance : " << balance;
-            }
+        friend istream &operator>>(istream &is, Account &acc)
+        {
+            is >> acc.accountNumber >> acc.balance >> acc.AccountHolder;
+            return is;
+        }
 
+        friend ostream &operator<<(ostream &os, Account &acc)
+        {
+            os << acc.accountNumber << "  " << acc.balance << "  " << acc.AccountHolder;
+            return os;
+        }
 
+        void display()
+        {
+            cout << "\n Account Number : " << accountNumber;
+            cout << "\n Account Holder : " << AccountHolder;
+            cout << "\n Balance : " << balance;
+        }
 
+        void serialization(ofstream &outFile)
+        {
+            outFile << accountNumber << endl;
+            outFile << AccountHolder << endl;
+            outFile << balance << endl;
+        }
+
+        void deserialization(ifstream &infile)
+        {
+            infile >> accountNumber;
+            infile.ignore();
+            getline(infile, AccountHolder);
+            infile >> balance;
+        }
     };
-void displayMenu()
-{
-    cout<<"\n******OPERATION*********";
-    cout<<"\n1. check Balance";
-    cout<<"\n2. Withdraw Money";
-    cout<<"\n3. Deposit Money";
-    cout<<"\n4. Display Customer details";
-    cout<<"\n5. Serialize your Data";
-    cout << "\n6.  Deserialize your Data";
-    cout<<"\n7. Exit";
-}
+
+    class RepoManager
+    {
+    public:
+        static Account currentAccount;
+
+        static void saveAccountDetails()
+        {
+            ofstream outfile("Account.dat");
+            if (outfile.is_open())
+            {
+                currentAccount.serialization(outfile);
+                cout << "\nAccount details saved successfully";
+                outfile.close();
+            }
+        }
+
+        static void getAccountDetails()
+        {
+            ifstream infile("Account.dat");
+            if (infile.is_open())
+            {
+                currentAccount.deserialization(infile);
+                cout << "\nAccount details get successfully";
+                infile.close();
+            }
+        }
+    };
+
+    Account RepoManager::currentAccount(78, 250.1, "sanika");
+    void displayMenu()
+    {
+        cout << "\n******OPERATION*********";
+        cout << "\n1. check Balance";
+        cout << "\n2. Withdraw Money";
+        cout << "\n3. Deposit Money";
+        cout << "\n4. Display Customer details";
+        cout << "\n5. Serialize your Data";
+        cout << "\n6.  Deserialize your Data";
+        cout << "\n7. Exit";
+    }
 
 }
 using namespace Bank;
@@ -100,7 +141,6 @@ int main()
     double amount;
     displayMenu();
     int choice;
-    Account acc(25, 250, "sanika");
 
     do
     {
@@ -111,27 +151,41 @@ int main()
         {
 
         case 1:
-            acc.checkBalance();
+            RepoManager::getAccountDetails();
+            RepoManager::currentAccount.checkBalance();
             break;
 
         case 2:
+            RepoManager::getAccountDetails();
             cout << "\nenter amount to withdraw: ";
             cin >> amount;
-            acc.withdraw(amount);
+            RepoManager::currentAccount.withdraw(amount);
+            RepoManager::saveAccountDetails();
+
             break;
 
         case 3:
+            RepoManager::getAccountDetails();
             cout << "\nenter amount to deposite: ";
             cin >> amount;
-            acc.deposit(amount);
+            RepoManager::currentAccount.deposit(amount);
+            RepoManager::saveAccountDetails();
             break;
 
         case 4:
             cout << "\nAccount holder Details: ";
-            acc.display();
+            RepoManager::currentAccount.display();
             break;
 
         case 5:
+            RepoManager::saveAccountDetails();
+            break;
+
+        case 6:
+            RepoManager::getAccountDetails();
+            break;
+
+        case 7:
             cout << "\nThank you ";
             break;
 
