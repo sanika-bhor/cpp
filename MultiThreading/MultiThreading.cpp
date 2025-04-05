9f24eeec381ab4910d7e62bb7c32f80b20bf2579
sanika-bhor <bhorsanika0239@gmail.com>
2025-04-05 16:06:12 +0530

    multithreading

---
+++ MultiThreading/MultiThreading.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <thread>
+#include<chrono>
+using namespace std;
+void display_hello()
+{
+    int i=0;
+    while (i<5)
+    {
+        cout << "Hello, World! from thread 1" << endl;
+        this_thread::sleep_for(chrono::seconds(3));
+        i++;
+    }
+}
+
+void display_bye()
+{
+    int i = 0;
+    while (i < 5)
+    {
+        cout << "Bye, World! from thread 2" << endl;
+        this_thread::sleep_for(chrono::seconds(7));
+        i++;
+    }
+}
+
+int main()
+{
+    cout<<"primary thread is started"<<endl;
+    thread t1(display_hello);
+    t1.join();
+    thread t2(display_bye);
+    t2.join();
+    cout << "primary thread is terminated" << endl;
+
+    return 0;
+}
