a9ebee85006cfd3721c7d51ded68b7fbe5784b7f
sanika-bhor <bhorsanika0239@gmail.com>
2025-11-11 23:16:04 +0530

    change

--- Spos/macro_combine.cpp
+++ Spos/macro_combine.cpp
@@ -1,14 +1,25 @@
-// INPUT :
-//  source.txt
+
+// INPUT
+// source.txt
 /*
 MACRO
-INCR &A, &B
-ADD &A, &B
+INCR
+MOVER AREG, DATA
+ADD AREG, ONE
+MOVEM AREG, DATA
 MEND
+
 START 100
-INCR AREG, BREG
+READ DATA
+INCR
+PRINT DATA
+STOP
+DATA DS 1
+ONE DC 1
 END
 */
+
+// macro_combined.cpp
 
 #include <bits/stdc++.h>
 using namespace std;
@@ -30,7 +41,7 @@ void pass1()
 
     if (!fin)
     {
-        cout << "Error: cannot open input file.\n";
+        cout << "Error: cannot open source.txt file\n";
         return;
     }
 
@@ -51,7 +62,7 @@ void pass1()
 
         if (inMacro && macroName.empty())
         {
-            macroName = word;
+            macroName = word; // macro name
             MNT.push_back({macroName, (int)MDT.size()});
             continue;
         }
@@ -75,53 +86,136 @@ void pass1()
         }
     }
 
-    // Write to MNT file
+    // Write MNT
     mntFile << "MNT\n----------------------------\n";
     for (auto &m : MNT)
         mntFile << "Macro: " << m.name << " -> MDT Index: " << m.mdtIndex << "\n";
 
-    // Write to MDT file
-    mdtFile << "MDT\n----------------------------\n";
+    // Write MDT
+    mdtFile << "MDT\n------------------------------\n";
     for (int i = 0; i < (int)MDT.size(); i++)
         mdtFile << i << ": " << MDT[i] << "\n";
 
     // Write Intermediate file
-    interFile << "Intermediate Code\n----------------------------\n";
+    interFile << "Intermediate code\n---------------------------\n";
     for (auto &line : INTERMEDIATE)
         interFile << line << "\n";
 
     cout << "\nPass 1 completed successfully!\n";
 }
 
+void pass2()
+{
+    ifstream interFile("intermediate.txt");
+    ofstream expandedFile("expanded.txt");
+
+    if (!interFile)
+    {
+        cout << "Error: cannot open intermediate.txt\n";
+        return;
+    }
+
+    cout << "\nPass 2: Expanded Code\n----------------------\n";
+    expandedFile << "Expanded Code\n----------------------\n";
+
+    string line, word;
+
+    // Skip header lines in intermediate.txt
+    getline(interFile, line); // skip "Intermediate code"
+    getline(interFile, line); // skip dashed line
+
+    while (getline(interFile, line))
+    {
+        stringstream ss(line);
+        ss >> word;
+
+        bool expanded = false;
+        for (auto &m : MNT)
+        {
+            if (word == m.name)
+            {
+                expanded = true;
+                int i = m.mdtIndex;
+                while (MDT[i] != "MEND")
+                {
+                    cout << MDT[i] << "\n";
+                    expandedFile << MDT[i] << "\n";
+                    i++;
+                }
+                break;
+            }
+        }
+
+        if (!expanded)
+        {
+            cout << line << "\n";
+            expandedFile << line << "\n";
+        }
+    }
+
+    cout << "\nPass 2 completed successfully! Expanded code written to expanded.txt\n";
+}
+
 int main()
 {
-    cout << "------ PASS 1: MACRO PROCESSOR ------\n";
+    cout << "------ TWO PASS MACRO PROCESSOR ------\n";
     pass1();
+    pass2();
     return 0;
 }
 
 // OUTPUT :
-// intermediate.txt
-/*
-Intermediate Code
-----------------------------
-
-START 100
-INCR AREG, BREG
-END
-*/
-
-// mdt.txt
-/*
-MDT
-----------------------------
-0: ADD &A, &B
-1: MEND
-*/
 
 // mnt.txt
+
 /*
 MNT
 ----------------------------
 Macro: INCR -> MDT Index: 0
+*/
+
+// mdt.txt
+
+/*
+MDT
+------------------------------
+0: MOVER AREG, DATA
+1: ADD AREG, ONE
+2: MOVEM AREG, DATA
+3: MEND
+*/
+
+// intermediate.txt
+
+/*
+Intermediate code
+---------------------------
+
+START 100
+READ DATA
+INCR
+PRINT DATA
+STOP
+DATA DS 1
+ONE DC 1
+END
+*/
+
+// expanded.txt
+
+/*
+Expanded Code
+----------------------
+
+START 100
+READ DATA
+MOVER AREG, DATA
+ADD AREG, ONE
+MOVEM AREG, DATA
+PRINT DATA
+STOP
+DATA DS 1
+ONE DC 1
+END
+
 */
